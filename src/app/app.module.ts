19939459e05c6d5fd45e8b5385460fc5c29a9a10
JWaldburger <49599965+JWaldburger@users.noreply.github.com>
2021-04-24 11:00:24 -0500

    reviewed code...
    Everything working so far...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserModule } from '@angular/platform-browser'
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
